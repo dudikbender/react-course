0af51712547a3f0074db8a9af1f745de58dc247f
David Bender <bender2242@gmail.com>
2022-02-05 01:04:50 +0000

    added db call for new meetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,12 +1,31 @@
-import NewMeetupForm from '../components/meetups/NewMeetupForm'
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  return (
-    <section>
-      <h1>Add New Meetup</h1>
-      <NewMeetupForm />
-    </section>
-  );
+	const history = useHistory();
+
+	function addMeetupHandler(meetupData) {
+		fetch(
+			'https://react-getting-started-65e1c-default-rtdb.firebaseio.com/meetups.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(meetupData),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		).then(() => {
+			history.replace('/');
+		});
+	}
+
+	return (
+		<section>
+			<h1>Add New Meetup</h1>
+			<NewMeetupForm onAddMeetup={addMeetupHandler} />
+		</section>
+	);
 }
 
 export default NewMeetupPage;
